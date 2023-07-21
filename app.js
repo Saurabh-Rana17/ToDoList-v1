9e6b6924bf535538389900683ee958f1bbf41d7f
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-21 16:15:56 +0530

    date format and code refactor

--- app.js
+++ app.js
@@ -4,31 +4,13 @@ const app = express();
 app.set('view engine','ejs')
 app.get("/", function (req, res) {
     var date = new Date();
-    var today = date.getDay();
-    var day =""
-    switch (today) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-                day = "Monay";
-            break;
-        case 2:
-                day = "Tuesday";
-            break;
-        case 3:
-                day = "Wednesday";
-            break;
-        case 4:
-                day = "Thursday";
-            break;
-        case 5:
-                day = "Friday";
-            break;
-        case 6:
-                day = "Saturday";
-            break;
-    }
+    var option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = date.toLocaleDateString("en-IN", option);
+    
     res.render("list", { KindOfDay: day });
 })
 app.listen(3000, function () {
