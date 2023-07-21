18795ebb65950a142760a2e76f538db4555a66fc
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-21 18:35:57 +0530

    ui completed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-var values = [];
+var values = ['one','two','three'];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+"/public"));
