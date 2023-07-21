f3fec4ac33f63c3dd0dfd6910ddd9240b1fe852c
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-21 19:39:17 +0530

    work list added

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-var values = ['one','two','three'];
+var values = ['one', 'two', 'three'];
+var workItem = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+"/public"));
@@ -18,17 +19,35 @@ app.get("/", function (req, res) {
     };
     var day = date.toLocaleDateString("en-IN", option);
     
-    res.render("list", { KindOfDay: day,newItems:values });
+    res.render("list", { listTitle: day, newItems: values });
+});
+app.get("/work", function (req,res) {
+    res.render("list",{listTitle:"Work ",newItems:workItem});
+});
+app.post("/work", function (req, res) {
+    let value = req.body.item;
+    workItem.push(value);
+    res.redirect("/work");
 })
+
+
+
+app.post("/", function (req, res) {
+    console.log(req.body);
+    let value = req.body.item;
+    
+    if (req.body.list == "Work") {
+        workItem.push(value);
+        res.redirect("/work");
+    } else {
+        values.push(value);
+    }
+    
+    values.push(value);
+    res.redirect("/");
+
+});
 
 app.listen(3000, function () {
     console.log("listening on 3000");
-})
-
-app.post("/", function (req,res) {
-    
-    var value = req.body.item;
-    values.push(value);
-    res.redirect("/"); 
-
-})
+});
