9ace0c0bfa52dec26e70b1773312150b5fdde03d
Saurabh-Rana17 <saurabhrana200317@gmail.com>
2023-07-29 21:39:28 +0530

    mongo

--- app.js
+++ app.js
@@ -2,53 +2,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const Date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
-var values = ["Study Front end ","Learn backend framework","study devops"];
+var values = ["Study Front end ", "Learn backend framework", "study devops"];
 var workItem = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname + "/public"));
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-    let day = Date();
+  let day = Date();
 
-    
-    
-    res.render("list", { listTitle: day, newItems: values });
+  res.render("list", { listTitle: day, newItems: values });
 });
-app.get("/work", function (req,res) {
-    res.render("list",{listTitle:"Work ",newItems:workItem});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work ", newItems: workItem });
 });
 app.get("/about", function (req, res) {
-    res.render("about");
-})
-
-app.post("/work", function (req, res) {
-    let value = req.body.item;
-    workItem.push(value);
-    res.redirect("/work");
-})
-
-
-
-app.post("/", function (req, res) {
-    // console.log(req.body);
-    let value = req.body.item;
-    
-    if (req.body.list == "Work") {
-        workItem.push(value);
-        res.redirect("/work");
-    } else {
-        values.push(value);
-    }
-    
-    values.push(value);
-    res.redirect("/");
-
+  res.render("about");
 });
 
-app.listen(process.env.PORT|| 3000, function () {
-    console.log("listening on 3000");
+app.post("/work", function (req, res) {
+  let value = req.body.item;
+  workItem.push(value);
+  res.redirect("/work");
+});
+
+app.post("/", function (req, res) {
+  // console.log(req.body);
+  let value = req.body.item;
+
+  if (req.body.list == "Work") {
+    workItem.push(value);
+    res.redirect("/work");
+  } else {
+    values.push(value);
+  }
+
+  values.push(value);
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("listening on 3000");
 });
